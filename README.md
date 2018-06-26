2123566603f96459c8864233c67ea069ae645de5
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2018-06-26 20:48:53 +0200

    add README, fix run.py

---
+++ README.md
@@ -0,0 +1,10 @@
+Major Depression as a Network: Complex System Simulation
+--------------------------------------------------------
+
+#### Requirements
+- Modern Webbrowser
+- Python 3.6
+- Websockets for Python: ```pip install websockets```
+
+#### Run
+Executing ```python run.py``` will start the web server, socket server and launch the website!
--- run.py
+++ run.py
@@ -9,4 +9,4 @@ SOCKET_SERVER = ['python', 'startbootstrap-clean-blog-gh-pages/server.py']
 threading.Thread(target=subprocess.call, args=(HTTP_SERVER,)).start()
 threading.Thread(target=subprocess.call, args=(SOCKET_SERVER,)).start()
 
-webbrowser.open('//localhost:8000/startbootstrap-clean-blog-gh-pages/', new=2)
+webbrowser.open_new_tab('http://localhost:8000/startbootstrap-clean-blog-gh-pages/')
