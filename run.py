c449a969a1205839d7e002b19e3908826f6faea1
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2018-06-28 14:43:22 +0200

    add minor fixes

--- run.py
+++ run.py
@@ -10,6 +10,6 @@ SOCKET_SERVER = ['python', 'startbootstrap-clean-blog-gh-pages/server.py']
 threading.Thread(target=subprocess.call, args=(SOCKET_SERVER,)).start()
 threading.Thread(target=subprocess.call, args=(HTTP_SERVER,)).start()
 
-sleep(1)
+sleep(1)  # Wait for Servers to Boot
 
 webbrowser.open_new_tab('http://localhost:8000/startbootstrap-clean-blog-gh-pages/')
