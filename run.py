9884e3adb7f2d9610b997347c81dfeeaad805baf
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2018-06-28 11:37:46 +0200

    fix padding issue

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import subprocess
 import threading
 import webbrowser
+from time import sleep
 
 
 HTTP_SERVER = ['python', '-m', 'http.server']
 SOCKET_SERVER = ['python', 'startbootstrap-clean-blog-gh-pages/server.py']
 
-threading.Thread(target=subprocess.call, args=(HTTP_SERVER,)).start()
 threading.Thread(target=subprocess.call, args=(SOCKET_SERVER,)).start()
+threading.Thread(target=subprocess.call, args=(HTTP_SERVER,)).start()
+
+sleep(1)
 
 webbrowser.open_new_tab('http://localhost:8000/startbootstrap-clean-blog-gh-pages/')
